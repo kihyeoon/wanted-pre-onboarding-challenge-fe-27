d51d792d4618fe55fda1e0bcbb284699a387a499
kihyeoon <kihyeoon@gmail.com>
2024-11-05 17:27:25 +0900

    add: react-router-dom add: shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <div className="text-3xl font-bold">Hello World</div>
+      <Button variant="outline">Click me</Button>
     </>
   );
 }
